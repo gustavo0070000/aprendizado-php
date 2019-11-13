c322434a549561a500f02e076affe089030e7d0c
gustavo0070000 <gubilionario@gmail.com>
2019-11-13 15:09:37 -0300

    Add files via upload

---
+++ Switch.php
@@ -0,0 +1,85 @@
+<?php 
+
+
+/* $vogais = "a e i o u";
+$consonantes = "b c d f g h j k l m n p q r s t v w x z .";*/
+$letra = readline ("digite uma letra: " ); 
+
+switch ($letra) {
+    case "a": echo "é uma vogal! ";
+    break;
+    case "e": echo "é uma vogal! ";
+    break;
+    case "i": echo "é uma vogal! ";
+    break;
+    case "o": echo "é uma vogal! ";
+    break;
+    case "u": echo "é uma vogal! ";
+    break;
+
+/*-----separação consoantes de vogais-----*/
+
+    case "b" : echo "é uma consoante ";
+    break;
+    case "c": echo "é uma consoante! ";
+    break;
+    case "d": echo "é uma consoante! ";
+    break;
+    case "f": echo "é uma consoante! ";
+    break;
+    case "g": echo "é uma consoante! ";    
+    break;
+    case "h": echo "é uma consoante! ";
+    break;
+    case "j": echo "é uma consoante! ";
+    break;
+    case "k": echo "é uma consoante! ";
+    break;
+    case "l": echo "é uma consoante! ";
+    break;
+    case "m": echo "é uma consoante! ";
+    break;
+    case "n": echo "é uma consoante! ";
+    break;
+    case "p": echo "é uma consoante! ";
+    break;
+    case "q": echo "é uma consoante! ";
+    break;
+    case "r": echo "é uma consoante! ";
+    break;
+    case "s": echo "é uma consoante! ";
+    break;
+    case "t": echo "é uma consoante! ";
+    break;
+    case "v": echo "é uma consoante! ";
+    break;
+    case "w": echo "é uma consoante! ";
+    break;
+    case "x": echo "é uma consoante! ";
+    break;
+    case "y": echo "é uma consoante! ";
+    break;
+    case "z": echo "é uma consoante! ";
+    break;
+
+    default : echo "não é uma letra";
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
