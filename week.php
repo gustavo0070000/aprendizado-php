0f84804f4270616e829f0187ea6ab1f108d3a2d2
gustavo0070000 <gubilionario@gmail.com>
2019-11-13 15:37:44 -0300

    Week praticas
    done

---
+++ week.php
@@ -0,0 +1,39 @@
+<?php 
+
+$usefullday = 2;
+$usefullday = 3;
+$usefullday = 4;
+$usefullday = 5;
+$usefullday = 6;
+
+$uselessday = 1;
+$uselessday = 7;
+
+$days = readline("digite um numero");
+
+switch($days){
+case $usefullday == $days: echo "é um dia útil";
+break;
+default : echo "não é um dia útil";
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
