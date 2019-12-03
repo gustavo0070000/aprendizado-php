0ef30074cafd2896e25fd5bd6fdb89fb7e9e5f91
gustavo0070000 <gubilionario@gmail.com>
2019-12-03 09:31:31 -0300

    Add files via upload

---
+++ parimpar.php
@@ -0,0 +1,25 @@
+<?php
+// $num = random_int(1, 100);;
+// $contador = 0;
+// while($contador<=100){
+//    if($num%2==0){
+//       echo "o número $num é par";
+//    }
+//    else{ 
+//        echo "o numero $num é impar";
+
+
+//    }
+// //$contador = $contador + 1;
+// break;
+// }
+
+
+$cont=0;
+
+while($cont<=100){
+    if($cont%2==0){
+        echo "$cont é par".PHP_EOL;
+    }
+$cont=$cont+1;
+}
