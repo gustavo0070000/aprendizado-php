96614e910a479974127663552304a4da5976fdd2
gustavo0070000 <gubilionario@gmail.com>
2019-12-09 11:14:48 -0300

    Add files via upload

---
+++ myfunc.php
@@ -0,0 +1,21 @@
+<?php
+$x = readline("digite um número");
+$y= readline("digite um  número limite");
+do{
+echo  $x ++ ;
+    //if($x<1){
+//         echo ("$x é menor que 1!");
+//     }else{ echo ("$x é maior que 1!") ;
+//     }
+// break;
+if ($x >= $y){
+    break;
+}
+
+}while($x>1);
+
+
+
+
+
+
